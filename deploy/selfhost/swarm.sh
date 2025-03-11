057fb0ec3b94b8474177dbe3e2c8afbf8f27d1d5
Phennapa Saeliw <psaeliw@gmail.com>
2025-03-11 13:56:40 +0700

    fix: update swarm.sh

--- deploy/selfhost/swarm.sh
+++ deploy/selfhost/swarm.sh
@@ -5,9 +5,9 @@ SERVICE_FOLDER=plane-app
 SCRIPT_DIR=$PWD
 PLANE_INSTALL_DIR=$PWD/$SERVICE_FOLDER
 export APP_RELEASE="stable"
-export DOCKERHUB_USER=makeplane
+export DOCKERHUB_USER=ghcr.io/phennapa/plane
 
-export GH_REPO=makeplane/plane
+export GH_REPO=phennapa/plane
 export RELEASE_DOWNLOAD_URL="https://github.com/$GH_REPO/releases/download"
 export FALLBACK_DOWNLOAD_URL="https://raw.githubusercontent.com/$GH_REPO/$BRANCH/deploy/selfhost"
 
